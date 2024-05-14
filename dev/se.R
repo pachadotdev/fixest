cd58b8d7fea15ed8ba7c6069b360a75f919d00a6
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-05-13 20:17:40 -0400

    tidy dev/ folder

--- dev/se.R
+++ dev/se.R
@@ -9,13 +9,13 @@ k = 1
 base = readRDS("dev/base.rds")
 
 # try to create a memory leak by repetition
-for (i in 1:10) {
+for (i in 1:3) {
   est_multi <- feols(c(y1, y2) ~ x1 + csw0(x2, x3) + x4 | species + fe2, base, fsplit = ~species)
 }
 # ok
 
 # try to create a memory leak by repetition
-for (i in 1:10) {
+for (i in 1:3) {
   mat <- coeftable(object = est_multi[[1]],
       vcov = NULL, ssc = NULL, cluster = "fe3",
       keep = NULL, drop = NULL, order = NULL)
